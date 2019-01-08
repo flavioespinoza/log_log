1a2e7833423f25efe793292f824d68a37f83d34c
Flavio Espinoza <flavio.espinoza@gmail.com>
2019-01-08 00:17:39 -0700

    formatting.

--- index.js
+++ index.js
@@ -7,77 +7,77 @@ const log = require('ololog').configure({locate: false})
 
 const _log = {
 	deep: (data) => {
-	console.log(util.inspect(data, false, null, true))
-},
-info: (msg) => {
-	if (typeof msg === 'object') {
-		msg = JSON.stringify(msg)
+		console.log(util.inspect(data, false, null, true))
+	},
+	info: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
+		console.log(` Info: ${msg} `.bgBlue.white)
+	},
+	error: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
+		console.log(` Error: ${msg} `.bgRed.white)
+	},
+	debug: (msg) => {
+		console.log(` Debug: ${msg} `.bgRed.white)
+	},
+	alert: (msg) => {
+		console.log(` Alert: ${msg} `.bgYellow.white)
+	},
+	warn: (msg) => {
+		console.log(` Warn: ${msg} `.bgMagenta.white)
+	},
+	blue: (msg) => {
+		console.log(`${msg} `.bgBlue.white)
+	},
+	red: (msg) => {
+		console.log(`${msg} `.bgRed.white)
+	},
+	green: (msg) => {
+		console.log(`${msg} `.bgRed.white)
+	},
+	pink: (msg) => {
+		console.log(`${msg} `.bgLightRed.white)
+	},
+	lime: (msg) => {
+		console.log(`${msg} `.bgLightGreen.white)
+	},
+	lemon: (msg) => {
+		console.log(`${msg} `.bgLightYellow.white)
+	},
+	baby: (msg) => {
+		console.log(`${msg} `.bgLightBlue.white)
+	},
+	violet: (msg) => {
+		console.log(`${msg} `.bgLightMagenta.white)
+	},
+	aqua: (msg) => {
+		console.log(`${msg} `.bgLightCyan.white)
+	},
+	gray: (msg) => {
+		console.log(`${msg} `.bgLightGray.black)
+	},
+	cyan: (msg) => {
+		console.log(`${msg} `.bgCyan.white)
+	},
+	assert: (item, item_name) => {
+		if (item) {
+			let msg = ` SUCCESS: ${item_name} = ${item} `
+			console.log(`ASSERT`, msg.bgCyan.black)
+		}
+		else {
+			let msg = ` FAIL: ${item_name} = ${item} `
+			console.log(`ASSERT`, msg.bgRed.black)
+		}
+	},
+	timer: (method, method_name) => {
+		console.time(`_timer ${method_name}()`)
+		method()
+		console.timeEnd(`_timer ${method_name}()`)
 	}
-	console.log(` Info: ${msg} `.bgBlue.white)
-},
-error: (msg) => {
-	if (typeof msg === 'object') {
-		msg = JSON.stringify(msg)
-	}
-	console.log(` Error: ${msg} `.bgRed.white)
-},
-debug: (msg) => {
-	console.log(` Debug: ${msg} `.bgRed.white)
-},
-alert: (msg) => {
-	console.log(` Alert: ${msg} `.bgYellow.white)
-},
-warn: (msg) => {
-	console.log(` Warn: ${msg} `.bgMagenta.white)
-},
-blue: (msg) => {
-	console.log(`${msg} `.bgBlue.white)
-},
-red: (msg) => {
-	console.log(`${msg} `.bgRed.white)
-},
-green: (msg) => {
-	console.log(`${msg} `.bgRed.white)
-},
-pink: (msg) => {
-	console.log(`${msg} `.bgLightRed.white)
-},
-lime: (msg) => {
-	console.log(`${msg} `.bgLightGreen.white)
-},
-lemon: (msg) => {
-	console.log(`${msg} `.bgLightYellow.white)
-},
-baby: (msg) => {
-	console.log(`${msg} `.bgLightBlue.white)
-},
-violet: (msg) => {
-	console.log(`${msg} `.bgLightMagenta.white)
-},
-aqua: (msg) => {
-	console.log(`${msg} `.bgLightCyan.white)
-},
-gray: (msg) => {
-	console.log(`${msg} `.bgLightGray.black)
-},
-cyan: (msg) => {
-	console.log(`${msg} `.bgCyan.white)
-},
-assert: (item, item_name) => {
-	if (item) {
-		let msg = ` SUCCESS: ${item_name} = ${item} `
-		console.log(`ASSERT`, msg.bgCyan.black)
-	}
-	else {
-		let msg = ` FAIL: ${item_name} = ${item} `
-		console.log(`ASSERT`, msg.bgRed.black)
-	}
-},
-timer: (method, method_name) => {
-	console.time(`_timer ${method_name}()`)
-	method()
-	console.timeEnd(`_timer ${method_name}()`)
-}
 }
 
-module.exports = { _log, log }
+module.exports = {_log, log}
