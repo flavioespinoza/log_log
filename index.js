a9a30b87a2e6e01a05d71e62a00966972ac8349e
Flavio Espinoza <flavio.espinoza@gmail.com>
2019-01-08 00:28:49 -0700

    added catch for typeof object.

--- index.js
+++ index.js
@@ -22,45 +22,87 @@ const _log = {
 		console.log(` Error: ${msg} `.bgRed.white)
 	},
 	debug: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(` Debug: ${msg} `.bgRed.white)
 	},
 	alert: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(` Alert: ${msg} `.bgYellow.white)
 	},
 	warn: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(` Warn: ${msg} `.bgMagenta.white)
 	},
 	blue: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgBlue.white)
 	},
 	red: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgRed.white)
 	},
 	green: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgRed.white)
 	},
 	pink: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgLightRed.white)
 	},
 	lime: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgLightGreen.white)
 	},
 	lemon: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgLightYellow.white)
 	},
 	baby: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgLightBlue.white)
 	},
 	violet: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgLightMagenta.white)
 	},
 	aqua: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgLightCyan.white)
 	},
 	gray: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgLightGray.black)
 	},
 	cyan: (msg) => {
+		if (typeof msg === 'object') {
+			msg = JSON.stringify(msg)
+		}
 		console.log(`${msg} `.bgCyan.white)
 	},
 	assert: (item, item_name) => {
