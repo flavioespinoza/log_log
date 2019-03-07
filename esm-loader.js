89d0d4c45c3ea1ac09866afd38a0103fcc9c470b
flavioespinoza <flavio.espinoza@gmail.com>
2019-03-07 03:48:51 -0700

    removed lodash to test loading it with ems

--- esm-loader.js
+++ esm-loader.js
@@ -1,9 +1,9 @@
-// You can also create another variable, not use "require" specifically, see https://youtu.be/JcZ-FzfDq8A?t=1880
-require = require('esm')(module/*, options*/);
+// You can also create another variable, not use "require" specifically,
+//see https://youtu.be/JcZ-FzfDq8A?t=1880
 
-// XXX More usages:
-// require = require('esm')(module, { await: true });
-// require = require('esm')(module, { cjs: false });
-// require = require('esm')(module, { mode: 'all' });
+require = require('esm')(module)
+require = require('esm')(module, { await: true })
+require = require('esm')(module, { cjs: true })
+// require = require('esm')(module, { mode: 'all' })
 
-module.exports = require('./index.js');
+module.exports = require('./index.js')
