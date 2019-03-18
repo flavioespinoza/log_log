98906edc42cc7ab713f8080a2cad9f687d4cb0ac
flavioespinoza <flavio.espinoza@gmail.com>
2019-03-18 08:47:38 -0600

    colors

--- es5/index.js
+++ es5/index.js
@@ -1,115 +1,153 @@
 "use strict";
-
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-require('ansicolor').nice;
-
-var _ = require('lodash');
-
-var log = require('ololog');
-
-var util = require('util');
-
-var _log = {
-  deep: function deep(data) {
-    var options = {
-      showHidden: false,
-      depth: null,
-      colors: true
-    };
-    var inspect = util.inspect(data, options);
-    console.log(inspect);
-  },
-  info: function info(msg) {
-    if (_typeof(msg) === 'object') {
-      msg = _.toString(msg);
-    }
-
-    console.log(" Info: ".concat(msg, " ").bgBlue.white);
-  },
-  error: function error(msg) {
-    log.lightYellow('ERROR: ', msg);
-  },
-  alert: function alert(msg) {
-    if (_typeof(msg) === 'object') {
-      msg = _.toString(msg);
-    }
-
-    console.log(" Alert: ".concat(msg, " ").bgYellow.black);
-  },
-  warn: function warn(msg) {
-    if (_typeof(msg) === 'object') {
-      msg = _.toString(msg);
-    }
-
-    console.log(" Warn: ".concat(msg, " ").bgMagenta.white);
-  },
-  blue: function blue(msg) {
-    if (_typeof(msg) === 'object') {
-      msg = _.toString(msg);
-    }
-
-    console.log("".concat(msg, " ").bgBlue.black);
-  },
-  red: function red(msg) {
-    if (_typeof(msg) === 'object') {
-      msg = _.toString(msg);
-    }
-
-    console.log("".concat(msg, " ").bgRed.black);
-  },
-  green: function green(msg) {
-    if (_typeof(msg) === 'object') {
-      msg = _.toString(msg);
-    }
-
-    console.log("".concat(msg, " ").bgGreen.black);
-  },
-  pink: function pink(msg) {
-    if (_typeof(msg) === 'object') {
-      msg = _.toString(msg);
-    }
-
-    console.log("".concat(msg, " ").bgLightRed.black);
-  },
-  yellow: function yellow(msg) {
-    if (_typeof(msg) === 'object') {
-      msg = _.toString(msg);
-    }
-
-    console.log("".concat(msg, " ").bgLightYellow.white);
-  },
-  violet: function violet(msg) {
-    if (_typeof(msg) === 'object') {
-      msg = _.toString(msg);
-    }
-
-    console.log("".concat(msg, " ").bgLightMagenta.black);
-  },
-  cyan: function cyan(msg) {
-    if (_typeof(msg) === 'object') {
-      msg = _.toString(msg);
-    }
-
-    console.log("".concat(msg, " ").bgCyan.black);
-  },
-  assert: function assert(item, item_name) {
-    if (item) {
-      var msg = " SUCCESS: ".concat(item_name, " = ").concat(item, " ");
-      console.log("ASSERT", msg.bgCyan.black);
-    } else {
-      var _msg = " FAIL: ".concat(item_name, " = ").concat(item, " ");
-
-      console.log("ASSERT", _msg.bgRed.black);
-    }
-  },
-  timer: function timer(method, method_name) {
-    console.time("Timer ".concat(method_name, "()"));
-    method();
-    console.timeEnd("Timer ".concat(method_name, "()"));
-  }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-module.exports = {
-  _log: _log,
-  log: log
+Object.defineProperty(exports, "__esModule", { value: true });
+const lodash_1 = __importDefault(require("lodash"));
+const util_1 = __importDefault(require("util"));
+require("colors");
+const log = {
+    blue: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.blue);
+    },
+    red: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.red);
+    },
+    green: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.green);
+    },
+    pink: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.red.dim);
+    },
+    yellow: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.yellow.bold);
+    },
+    violet: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.magenta.dim);
+    },
+    magenta: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.magenta.bold);
+    },
+    cyan: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.cyan);
+    }
 };
+exports.log = log;
+const _log = {
+    deep: (data) => {
+        const options = {
+            showHidden: false,
+            depth: null,
+            colors: true
+        };
+        const inspect = util_1.default.inspect(data, options);
+        console.log(inspect);
+    },
+    info: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(` Info: ${msg} `.bgBlue);
+    },
+    error: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(` ERROR: ${msg} `.yellow);
+    },
+    alert: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(` Alert: ${msg} `.bgYellow);
+    },
+    warn: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(` Warn: ${msg} `.bgMagenta);
+    },
+    blue: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.bgBlue);
+    },
+    red: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.bgRed);
+    },
+    green: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.bgGreen);
+    },
+    pink: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.bgRed);
+    },
+    yellow: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.bgYellow);
+    },
+    violet: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.bgMagenta);
+    },
+    cyan: (msg) => {
+        if (typeof msg === 'object') {
+            msg = lodash_1.default.toString(msg);
+        }
+        console.log(`${msg} `.bgCyan);
+    },
+    assert: (item, item_name) => {
+        if (item) {
+            let msg = ` SUCCESS: ${item_name} = ${item} `;
+            console.log(`ASSERT`, msg.bgCyan);
+        }
+        else {
+            let msg = ` FAIL: ${item_name} = ${item} `;
+            console.log(`ASSERT`, msg.bgRed);
+        }
+    },
+    timer: (method, method_name) => {
+        console.time(`Timer ${method_name}()`);
+        method();
+        console.timeEnd(`Timer ${method_name}()`);
+    }
+};
+exports.default = _log;
+//# sourceMappingURL=index.js.map
